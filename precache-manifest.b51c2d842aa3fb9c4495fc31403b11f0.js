a6584273f40b973065eec71d25abe445755230bf
Olivia Ho <oliviaho@nevada.unr.edu>
2019-08-29 10:34:39 -0700

    Updates

---
+++ precache-manifest.b51c2d842aa3fb9c4495fc31403b11f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5faea400913e24dec23015833da37428",
+    "url": "/buffalo-watch/index.html"
+  },
+  {
+    "revision": "5f4c08564c001ea7bd65",
+    "url": "/buffalo-watch/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "5eba5414c61795f61f53",
+    "url": "/buffalo-watch/static/css/main.3ef85b2c.chunk.css"
+  },
+  {
+    "revision": "5f4c08564c001ea7bd65",
+    "url": "/buffalo-watch/static/js/2.739fda78.chunk.js"
+  },
+  {
+    "revision": "5eba5414c61795f61f53",
+    "url": "/buffalo-watch/static/js/main.ab705eeb.chunk.js"
+  },
+  {
+    "revision": "edb722ef04d773fd4761",
+    "url": "/buffalo-watch/static/js/runtime~main.34338215.js"
+  },
+  {
+    "revision": "38736150116e4cb9b0ce58620c84fa6b",
+    "url": "/buffalo-watch/static/media/stock.38736150.png"
+  }
+]);
